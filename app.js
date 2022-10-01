984316f226c4d21a025d71f37b0d51889179cdb5
לירז גנון <lirazganon@gmail.com>
2022-10-01 23:15:14 +0300

    initial commit

--- app.js
+++ app.js
@@ -1,51 +1,55 @@
 const { Client, Intents } = require("discord.js");
-const client = new Client({ intents: new Intents(98303) });
+const client = new Client({ intents: new Intents(3243773) });
 
-const prefix = "?";
+const gUser = {}
+const gMsg = [
+  'איך תרצה שנקרא לך?',
+  `מהו הנושא שתרצה לפרסם בו?\n
+  \t2️⃣ Javascrip \n
+  \t3️⃣ Python\n 
+  \t4️⃣ C++\n `,
+  'מהי הכותרת של הפוסט שלך?',
+  'מהו תוכן הפוסט?',
+  `תודה לך! אלו הפרטים שנקלטו:\n`
+]
+
+let gUserInput = true
+let currOpt = 0
+
 
 client.on("ready", () => {
   console.log("im online");
 });
 
 client.on("messageCreate", async (msg) => {
-  if (msg.content.startsWith(prefix)) {
-    msg.channel.send("command detected");
-    //msg.channel.permissionOverwrites.edit("1010988976066265329", { SEND_MESSAGES: false }).then(() => msg.channel.send("success"));
+  if (msg.author.id === '1025672251850375188') return
+  // console.log(msg);
+  // console.log(msg.content);
 
-    // const general = await msg.guild.channels.cache.get("987667090611789846");
-    // general.permissionOverwrites.edit("1010988976066265329", { SEND_MESSAGES: false }).then(() => msg.channel.send("success"));
-    // console.log(msg.guild);
-    const channels = await msg.guild.channels.cache;
-    const readOnlyChannels = ["998682973513728230", "999736957615755385"];
-    let check;
-    for (const channel of channels) {
-      check = true;
-      for (let i = 0; i < readOnlyChannels.length; i++) {
-        if (channel[0] == readOnlyChannels[i]) {
-          check = false;
-        }
-      }
-
-      if (check && channel[1].type != "GUILD_CATEGORY" && channel[1].type != "GUILD_NEWS_THREAD" && channel[1].type != "GUILD_PUBLIC_THREAD" && channel[1].type != "GUILD_PRIVATE_THREAD" ) {
-        console.log("1");
-        msg.channel.send("permissions for " + channel[0] + " has changed");
-        const overwrites = await channel[1].permissionOverwrites.cache;
-        await console.log(overwrites)
-        if (overwrites.size == 0) {
-          channel[1].permissionOverwrites.edit("998682692390506516", { VIEW_CHANNEL: true });
-          console.log("lo yarutz");
-        } else if (overwrites.size == 1) {
-          overwrites.first().edit({ VIEW_CHANNEL: true });
-        } else {
-          for (let overwrite of overwrites) {
-            if (overwrite[1].id != "998682692390506516") {
-              overwrite[1].edit({ VIEW_CHANNEL: true });
-            }
-          }
-        }
-      }
+  if (!currOpt) {
+    if (msg.content === '%') {
+      msg.channel.send(gMsg[currOpt])
+      currOpt++
+      return
+    } else {
+      // msg.channel.send('invalid prefix')
+      return
     }
   }
-});
 
-client.login("MTAxMTAzNjY4OTA1NTEwMTA1OA.GYsav9.bf1iKPvNb2EIuJTssBAPIQ_bbwD8hieisvKjgU");
+  gUser[currOpt] = msg.content
+  msg.channel.send(gMsg[currOpt])
+  currOpt++
+
+  if (gMsg.length === currOpt) {
+    currOpt = 0
+    msg.channel.send(JSON.stringify(gUser))
+    const attachments = JSON.parse(JSON.stringify(msg.attachments))
+    if (attachments.length > 0){
+      msg.channel.send(`You are so hot!!\n ${attachments[0].attachment}`)
+    }
+  }
+
+})
+
+client.login("MTAyNTY3MjI1********************************");
