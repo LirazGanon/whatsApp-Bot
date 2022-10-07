ac2dce307c4ff504b73b8649f08ba21edbe3d990
Amit.JoYo <pashoshdrag@gmail.com>
2022-10-07 12:15:30 +0300

    added state machine. saves temp clients name added functionality

--- app.js
+++ app.js
@@ -1,61 +1,123 @@
 const { Client, Intents, User } = require("discord.js");
 const client = new Client({ intents: new Intents(3243773) });
 
-const gMap = new Map();
+const gInProggres = new Map();
+const gClients = new Map();
 const gUserResponse = {}
 const gUser = {}
-const prefixMsg = `היי, 
-אני דבי, הבוט של
- Dev Online`
-
-const gMsg = [
-  'מי אתה?',
-  `מהו הנושא שתרצה לפרסם בו?\n
-  \t2️⃣ Javascrip \n
-  \t3️⃣ Python\n 
-  \t4️⃣ C++\n `,
-  'מהי הכותרת של הפוסט שלך?',
-  'מהו תוכן הפוסט?',
-  `תודה לך! אלו הפרטים שנקלטו:\n`
-]
 
 let gUserInput = true
 let currOpt = 0
 
 
+const State = {
+  New: 0,
+  Idle: 1,
+  InPost: 2
+}
+const welcomeMsg = `היי, 
+אני דבי, הבוט של
+Dev Online`
+
+const letsMeetMsg = "נעים להכיר, ומי אתם?"
+
+const letsStartMsg = `כדי להתחיל לרשום פוסט שלחו #start\n
+לעזרה בכל שלב שלחו #help`
+
+const optionsMsg = `להתחיל לרשום פוסט - $start \n
+לביטול #stop \n
+להצגת התפריט הזה #help`
+const stopProggress = `אין בעיה, ביטלתי את הפוסט`
+const gMsg = [
+  `מהו הנושא שתרצה לפרסם בו?\n
+\t2️⃣ Javascrip \n
+\t3️⃣ Python\n 
+\t4️⃣ C++\n `,
+  'מהי הכותרת של הפוסט שלך?',
+  'מהו תוכן הפוסט?',
+  `תודה לך! אלו הפרטים שנקלטו:\n`
+]
+
 client.on("ready", () => {
   console.log("im online");
-});
+})
 
 client.on("messageCreate", async (msg) => {
-
-  if (msg.content === '%') {
-    gMap.set(msg.author.id, { currOpt: 0, answers: {} });
-    msg.channel.send(prefixMsg)
-    msg.channel.send(gMsg[gMap.get(msg.author.id).currOpt++])
+  if (msg.author.id === '1025672251850375188') return
+  if ((msg.channel.parent == null) || (msg.channel.parent.name == null)) return
+  if (!(msg.channel.parent.name.toLowerCase() === `whatsapp`)) return
+  let userId = msg.author.id;
+  if (!gClients.has(userId)) {
+    gClients.set(userId, { clientName: ``, state: State.New })
+    welcomeTxt = welcomeMsg
+    msg.channel.send(welcomeMsg)
+    msg.channel.send(letsMeetMsg)
     return
   }
 
-  try {
-    const currOpt = gMap.get(msg.author.id).currOpt
-    gMap.get(msg.author.id).answers[currOpt] = msg.content
-    msg.channel.send(gMsg[gMap.get(msg.author.id).currOpt++])
-
-    if (gMsg.length === gMap.get(msg.author.id).currOpt) {
-
-      msg.channel.send(JSON.stringify(gMap.get(msg.author.id).answers))
-      const attachments = JSON.parse(JSON.stringify(msg.attachments))
-      if (attachments.length > 0) {
-        console.log(attachments[0].url)
-        gMap.get(msg.author.id).attachments = (attachments[0].url)
-        msg.channel.send(JSON.stringify(gMap.get(msg.author.id).attachments))
+  switch (msg.content.toLowerCase()) {
+    case `#start`:
+      {
+        gInProggres.set(userId, { currOpt: 0, answers: {} });
+        gClients.get(userId).state = State.InPost
+        break
       }
-      gMap.delete(msg.author.id)
-   
-    }
-  } catch (err) {
-    return
-  } 
+    case `#stop`:
+      {
+        gInProggres.delete(userId)
+        gClients.get(userId).state = State.Idle
+        msg.channel.send(stopProggress)
+        return
+        break
+      }
+    case `#help`:
+      {
+        msg.channel.send(optionsMsg)
+        break
+      }
+  }
+
+  switch (gClients.get(userId).state) {
+    case State.New:
+      {
+        gClients.get(userId).clientName = msg.content
+        gClients.get(userId).state = State.Idle
+
+        msg.channel.send(letsStartMsg)
+        break
+      }
+    case State.Idle:
+      {
+        msg.channel.send(`היי ${gClients.get(userId).clientName}`)
+        msg.channel.send(letsStartMsg)
+        break
+      }
+    case State.InPost:
+      {
+        try {
+          const currOpt = gInProggres.get(userId).currOpt
+          gInProggres.get(userId).answers[currOpt] = msg.content
+          msg.channel.send(gMsg[gInProggres.get(userId).currOpt++])
+
+          if (gMsg.length === gInProggres.get(userId).currOpt) {
+
+            msg.channel.send(JSON.stringify(gInProggres.get(userId).answers))
+            const attachments = JSON.parse(JSON.stringify(msg.attachments))
+            if (attachments.length > 0) {
+              console.log(attachments[0].url)
+              gInProggres.get(userId).attachments = (attachments[0].url)
+              msg.channel.send(JSON.stringify(gInProggres.get(userId).attachments))
+            }
+            gInProggres.delete(userId)
+            gClients.get(userId).state = State.Idle
+          }
+        } catch (err) {
+          return
+        }
+        break
+      }
+  }
+  
 })
 
-client.login("**********");
+client.login("<Your login info here>")
